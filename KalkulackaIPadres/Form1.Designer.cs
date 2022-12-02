b17761dc0b2e4516dc0cbe4294d74e7a288f7ab1
VojtechMensik <86232462+VojtechMensik@users.noreply.github.com>
2022-12-02 12:57:39 +0100

    Add files via upload

---
+++ KalkulackaIPadres/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace KalkulackaIPadres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxVstup = new System.Windows.Forms.ComboBox();
+            this.comboBoxIp = new System.Windows.Forms.ComboBox();
+            this.comboBoxVystup = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBoxVstup = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxVystup = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxVstup
+            // 
+            this.comboBoxVstup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxVstup.FormattingEnabled = true;
+            this.comboBoxVstup.Items.AddRange(new object[] {
+            "Decimální",
+            "Binární",
+            "Hexadecimální"});
+            this.comboBoxVstup.Location = new System.Drawing.Point(12, 28);
+            this.comboBoxVstup.Name = "comboBoxVstup";
+            this.comboBoxVstup.Size = new System.Drawing.Size(150, 21);
+            this.comboBoxVstup.TabIndex = 0;
+            this.comboBoxVstup.SelectedIndexChanged += new System.EventHandler(this.comboBoxVstup_SelectedIndexChanged);
+            // 
+            // comboBoxIp
+            // 
+            this.comboBoxIp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxIp.FormattingEnabled = true;
+            this.comboBoxIp.Items.AddRange(new object[] {
+            "IPv4",
+            "IPv6"});
+            this.comboBoxIp.Location = new System.Drawing.Point(246, 28);
+            this.comboBoxIp.Name = "comboBoxIp";
+            this.comboBoxIp.Size = new System.Drawing.Size(150, 21);
+            this.comboBoxIp.TabIndex = 1;
+            this.comboBoxIp.SelectedIndexChanged += new System.EventHandler(this.comboBoxIp_SelectedIndexChanged);
+            // 
+            // comboBoxVystup
+            // 
+            this.comboBoxVystup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxVystup.FormattingEnabled = true;
+            this.comboBoxVystup.Items.AddRange(new object[] {
+            "Decimální",
+            "Binární",
+            "Hexadecimální"});
+            this.comboBoxVystup.Location = new System.Drawing.Point(478, 28);
+            this.comboBoxVystup.Name = "comboBoxVystup";
+            this.comboBoxVystup.Size = new System.Drawing.Size(150, 21);
+            this.comboBoxVystup.TabIndex = 2;
+            this.comboBoxVystup.SelectedIndexChanged += new System.EventHandler(this.comboBoxVystup_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Převod ze soustavy:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Druh IP adresy:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(475, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Převod do soustavy:";
+            // 
+            // maskedTextBoxVstup
+            // 
+            this.maskedTextBoxVstup.Location = new System.Drawing.Point(12, 114);
+            this.maskedTextBoxVstup.Name = "maskedTextBoxVstup";
+            this.maskedTextBoxVstup.Size = new System.Drawing.Size(616, 20);
+            this.maskedTextBoxVstup.TabIndex = 6;
+            // 
+            // textBoxVystup
+            // 
+            this.textBoxVystup.Location = new System.Drawing.Point(12, 165);
+            this.textBoxVystup.Name = "textBoxVystup";
+            this.textBoxVystup.ReadOnly = true;
+            this.textBoxVystup.Size = new System.Drawing.Size(616, 20);
+            this.textBoxVystup.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Vstup:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Vystup:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 207);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxVystup);
+            this.Controls.Add(this.maskedTextBoxVstup);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxVystup);
+            this.Controls.Add(this.comboBoxIp);
+            this.Controls.Add(this.comboBoxVstup);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(659, 246);
+            this.MinimumSize = new System.Drawing.Size(659, 246);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Kalkulačka IP adres";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxVstup;
+        private System.Windows.Forms.ComboBox comboBoxIp;
+        private System.Windows.Forms.ComboBox comboBoxVystup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxVstup;
+        private System.Windows.Forms.TextBox textBoxVystup;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ KalkulackaIPadres/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KalkulackaIPadres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NastavMasku()
+        {
+            //ipv4 255.255.255.255 1(8).1(8).1(8).1(8) ff.ff.ff.ff
+            //ipv6 ffff::ffff::ffff::ffff::ffff::ffff::ffff::ffff
+            //     65535.65535.65535.65535.65535.65535.65535.65535
+            //     1(16)
+            string maska = "";
+            int poc = 2;
+            int del = 1;
+            char znak = '0';
+            if (comboBoxIp.SelectedIndex == 1)
+            {
+                del = 2;
+                poc = 3;
+            }
+            switch (comboBoxVstup.SelectedIndex)
+            {
+                case 0:
+                    //decimální
+                    del *= 3;
+                    del = del - del / 5;
+                    break;
+                case 1:
+                    //binární
+                    del *= 8;
+                    break;
+                case 2:
+                    //hexadecimální
+                    del *= 2;
+                    znak = '&';
+                    break;
+            }
+            for (int i = 0; i < del; i++)
+                maska += znak;
+            for (int i = 0; i < poc; i++)
+                maska += "." + maska;
+            maskedTextBoxVstup.Mask = maska;
+        }
+        private string NaBinarni(int cislo)
+        {
+            int delitel = 1;
+            string binarni = "";
+            while (delitel < cislo)
+                delitel *= 2;
+            while (delitel > 1)
+            {
+                if (cislo / delitel == 1)
+                {
+                    cislo = cislo % delitel;
+                    binarni += "1";
+                }
+                else
+                {
+                    binarni += "0";
+                }
+                delitel /= 2;
+            }
+            if (cislo == 1)
+                binarni += "1";
+            else
+                binarni += "0";
+            if (binarni[0] == '0')
+                binarni = binarni.Remove(0, 1);
+            return binarni;
+        }
+        private int NaInt(string binarni)
+        {
+            int cislo = 0;
+            for (int i = 0; i < binarni.Length; i++)
+            {
+                if (binarni[i] == '1')
+                    cislo += (int)Math.Pow(2, i);
+            }
+            return cislo;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxIp.SelectedIndex = 0;
+            comboBoxVstup.SelectedIndex = 0;
+        }
+
+        private void comboBoxVstup_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NastavMasku();
+        }
+
+        private void comboBoxVystup_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!maskedTextBoxVstup.Text.Contains(" "))
+            {
+                bool pokracovat = true;
+                int limit = 255;
+                textBoxVystup.Clear();
+                int[] cisla;
+                string[] vstup = maskedTextBoxVstup.Text.Split(',');
+                cisla = new int[vstup.Length];
+                switch (comboBoxVstup.SelectedIndex)
+                {
+                    case 1:
+                        //binární
+                        string s = maskedTextBoxVstup.Text;
+                        s = s.Replace(",", "");
+                        s = s.Replace("0", "");
+                        s = s.Replace("1", "");
+                        if (s.Length == 0)
+                        {
+                            for (int i = 0; i < vstup.Length; i++)
+                                cisla[i] = NaInt(vstup[i]);
+                        }
+                        else
+                        {
+                            pokracovat = false;
+                            textBoxVystup.Text = "Binární hodnota může mít jen znaky 0 nebo 1";
+                        }
+                        break;
+                    //decimální //hexa
+                    case 0:
+                        for (int i = 0; i < vstup.Length; i++)
+                            cisla[i] = int.Parse(vstup[i]);
+                        break;
+                    case 2:
+                        try
+                        {
+                            for (int i = 0; i < vstup.Length; i++)
+                                cisla[i] = int.Parse(vstup[i], NumberStyles.HexNumber);
+                        }
+                        catch
+                        {
+                            pokracovat = false;
+                            textBoxVystup.Text = "Hexadecimální hodnota musí být v rozsahu 0 až F, nejsou povolené speciální znaky";
+                        }
+                        break;
+                }
+                if (comboBoxIp.SelectedIndex == 1)
+                    limit = 65535;
+                foreach (int i in cisla)
+                {
+                    if (i > limit)
+                    {
+                        pokracovat = false;
+                        textBoxVystup.Text = "IP adresa není ve správným rozsahu";
+                    }
+                }
+                if (pokracovat)
+                {
+                    switch (comboBoxVystup.SelectedIndex)
+                    {
+                        case 1:
+                            foreach (int i in cisla)
+                                textBoxVystup.Text += NaBinarni(i) + ".";
+                            textBoxVystup.Text = textBoxVystup.Text.Remove(textBoxVystup.Text.Length - 1);
+                            break;
+                        case 0:
+                            foreach (int i in cisla)
+                                textBoxVystup.Text += i + ".";
+                            textBoxVystup.Text = textBoxVystup.Text.Remove(textBoxVystup.Text.Length - 1);
+                            break;
+                        case 2:
+                            foreach (int i in cisla)
+                                textBoxVystup.Text += i.ToString("X").ToLower() + "::";
+                            textBoxVystup.Text = textBoxVystup.Text.Remove(textBoxVystup.Text.Length - 2);
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                textBoxVystup.Text = "Musí být vyplněna maska";
+            }
+        }
+
+        private void comboBoxIp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NastavMasku();
+        }
+    }
+}
